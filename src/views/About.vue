cf419865dbf8784b4d5e221990fb150c51398a5d
josephrt <josephrt@yandex.ru>
2022-03-10 15:24:26 +0300

    file2

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,5 +2,6 @@
   <div class="about pa-6">
     <h1>About Page</h1>
     <p>Простое веб-приложение, реализвованное при помощи Vue.js и Vuetify</p>
+    <p>Простое веб-приложение, реализвованное при помощи Vue.js и Vuetify</p>
   </div>
 </template>
